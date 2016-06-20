c196d8a08f3c15bd68182bc369ef882b2be66221
Kyungjik Min <dp.min@lge.com>
2016-06-20 17:14:28 +0900

    modify trainingset for test

--- trainer.py
+++ trainer.py
@@ -8,6 +8,7 @@ import sys
 import time
 
 # Import data
+import numpy as np
 import tensorflow as tf
 import trainingset as ts
 
@@ -25,12 +26,12 @@ FLAGS = flags.FLAGS
 flags.DEFINE_string('data_dir', '/tmp/data/', 'Directory for storing data')
 flags.DEFINE_float('learning_rate', 0.001, 'Learning rate')
 flags.DEFINE_float('dropout_rate', 0.9, 'Dropout rate')
-flags.DEFINE_integer('training_repeat', 1, 'Training repeat count')
-flags.DEFINE_integer('batch_size', 10, 'Batch size per loop')
+flags.DEFINE_integer('training_repeat', 1000, 'Training repeat count')
+flags.DEFINE_integer('batch_size', 50, 'Batch size per loop')
 
 sess = tf.InteractiveSession()
 
-chart = tf.placeholder(tf.float32, [None, ts.CHART_SIZE, 7, 1])
+chart = tf.placeholder(tf.float32, [None, ts.CHART_SIZE, 1, 5])
 tf.histogram_summary("chart", chart)
 
 #print(ts.CHART_SIZE)
@@ -41,34 +42,41 @@ tf.histogram_summary("label", ratio)
 dropout_rate = tf.placeholder(tf.float32)
 batch_set_size = tf.placeholder(tf.int32)
 
-cw0 = tf.Variable(tf.random_normal([3, 7, 1 ,32], stddev=0.01))
+cw0 = tf.Variable(tf.random_normal([3, 1, 5,32], stddev=0.01))
 cL0r = tf.nn.relu(tf.nn.conv2d(chart, cw0, strides=[1, 1, 1, 1], padding='SAME')) # strides = [1, strideX, strideY, 1], padding=SAME or VALID
-cL0p = tf.nn.max_pool(cL0r, ksize=[1, 361, 7, 1], strides=[1, 361, 1, 1], padding='SAME')
-cL0 = tf.nn.dropout(cL0p, dropout_rate)
+cL0p = tf.nn.max_pool(cL0r, ksize=[1, 5, 1, 1], strides=[1, 5, 1, 1], padding='SAME')
+cL0 = tf.nn.dropout(cL0r, dropout_rate)
 
 #print(cL0)
 
-cw1 = tf.Variable(tf.random_normal([3, 7, 32, 64], stddev=0.01))
+cw1 = tf.Variable(tf.random_normal([3, 1, 32, 64], stddev=0.01))
 cL1r = tf.nn.relu(tf.nn.conv2d(cL0, cw1, strides=[1, 1, 1, 1], padding='SAME')) # strides = [1, strideX, strideY, 1], padding=SAME or VALID
-cL1p = tf.nn.max_pool(cL1r, ksize=[1, 5, 7, 1], strides=[1, 5, 1, 1], padding='SAME')
+cL1p = tf.nn.max_pool(cL1r, ksize=[1, 5, 1, 1], strides=[1, 5, 1, 1], padding='SAME')
 cL1 = tf.nn.dropout(cL1p, dropout_rate)
 
 #print(cL1)
 
-cw2 = tf.Variable(tf.random_normal([3, 7, 64, 128], stddev=0.01))
+cw2 = tf.Variable(tf.random_normal([3, 1, 64, 128], stddev=0.01))
 cL2r = tf.nn.relu(tf.nn.conv2d(cL1, cw2, strides=[1, 1, 1, 1], padding='SAME')) # strides = [1, strideX, strideY, 1], padding=SAME or VALID
-cL2p = tf.nn.max_pool(cL2r, ksize=[1, 4, 7, 1], strides=[1, 4, 1, 1], padding='SAME')
-cL2 = tf.nn.dropout(cL2p, dropout_rate)
+cL2p = tf.nn.max_pool(cL2r, ksize=[1, 5, 1, 1], strides=[1, 5, 1, 1], padding='SAME')
+cL2 = tf.nn.dropout(cL2r, dropout_rate)
 
 #print(cL2)
 
+cw3 = tf.Variable(tf.random_normal([3, 1, 128, 128], stddev=0.01))
+cL3r = tf.nn.relu(tf.nn.conv2d(cL2, cw3, strides=[1, 1, 1, 1], padding='SAME')) # strides = [1, strideX, strideY, 1], padding=SAME or VALID
+cL3p = tf.nn.max_pool(cL3r, ksize=[1, 5, 1, 1], strides=[1, 5, 1, 1], padding='SAME')
+cL3 = tf.nn.dropout(cL3p, dropout_rate)
+
+#print(cL3)
+
 # Create the model
 with tf.name_scope("input_layer"):
-    w0 = tf.get_variable("input_layer_weight", shape=[3*7*128, 256], initializer=xavier_init(3*7*128, 256))
+    w0 = tf.get_variable("input_layer_weight", shape=[867*128, 256], initializer=xavier_init(867*128, 256))
     #tf.histogram_summary("input_layer_weight", w0)    
     b0 = tf.Variable(tf.random_normal([256]))
     tf.histogram_summary("input_layer_bias", b0)
-    cL2 = tf.reshape(cL2, tf.pack([batch_set_size, -1]))
+    cL2 = tf.reshape(cL3, tf.pack([batch_set_size, -1]))
     L0r = tf.nn.relu(tf.matmul(cL2, w0) + b0)
     L0 = tf.nn.dropout(L0r, dropout_rate)
 
@@ -120,6 +128,13 @@ with tf.name_scope("train"):
     # train_step = tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(loss)
     train_step = tf.train.RMSPropOptimizer(FLAGS.learning_rate, 0.9).minimize(loss)
 
+def checkAccuracy():
+    #correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(batch_labels, 1))
+    correct_prediction = tf.less_equal(tf.abs(tf.argmax(prediction, 1) - tf.argmax(ratio, 1)), 0)
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print("accuracy= {:.9f}".format(accuracy.eval({chart: test_charts, ratio: test_labels, dropout_rate: 1, batch_set_size: 50})))
+    print
+    
 merged = tf.merge_all_summaries()
 writer = tf.train.SummaryWriter("/tmp/trainer_logs", sess.graph)
 
@@ -130,6 +145,13 @@ tf.initialize_all_variables().run()
 
 ts.shuffle_batch_set()
 
+np.set_printoptions(threshold=np.nan, linewidth=200)
+#batch_charts, batch_labels = ts.next_batch(FLAGS.batch_size)
+#print(batch_labels[:,30:])
+
+# Test trained model
+test_charts, test_labels = ts.next_test_batch(50)
+
 total_batch = int(ts.num_set / FLAGS.batch_size)
 for repeats in range(FLAGS.training_repeat):
 
@@ -137,27 +159,21 @@ for repeats in range(FLAGS.training_repeat):
 
     print("Epoch:", "{:04d} Training...".format(repeats+1))
     #for batch_set in range(total_batch):
-    for batch_set in range(100):
-        batch_charts, batch_labels = ts.next_batch(FLAGS.batch_size)
+    for batch_set in range(1):
+        batch_charts, batch_labels = ts.next_batch(FLAGS.batch_size, True)
         train_step.run({chart: batch_charts, ratio: batch_labels, dropout_rate: FLAGS.dropout_rate, batch_set_size: FLAGS.batch_size})
         # compute average loss
-        avg_loss += loss.eval({chart: batch_charts, ratio: batch_labels, dropout_rate: FLAGS.dropout_rate, batch_set_size: FLAGS.batch_size}) / 5 #total_batch
+        avg_loss += loss.eval({chart: batch_charts, ratio: batch_labels, dropout_rate: FLAGS.dropout_rate, batch_set_size: FLAGS.batch_size}) / 1 #total_batch
         if batch_set % 10 == 0 and batch_set > 2: 
             print("...{:.2f}%".format((batch_set+1)/total_batch*100.0))
             summary = sess.run(merged, feed_dict={chart: batch_charts, ratio: batch_labels, dropout_rate: FLAGS.dropout_rate, batch_set_size: FLAGS.batch_size})
             writer.add_summary(summary, repeats*5+batch_set)
     
     print("loss=", "{:.9f}".format(avg_loss))
+    #checkAccuracy()
     #summary = sess.run(merged, feed_dict={x: batch_xs, y_: batch_ys, dropout_rate: FLAGS.dropout_rate})
     #writer.add_summary(summary, repeats)
 
+checkAccuracy()
 
-# Test trained model
-ts.shuffle_batch_set()
-test_charts, test_labels = ts.next_batch(50, True)
-
-#correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(test_labels, 1))
-correct_prediction = tf.less_equal(tf.abs(tf.argmax(prediction, 1) - tf.argmax(test_labels, 1)), 1)
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(accuracy.eval({chart: test_charts, ratio: test_labels, dropout_rate: 1, batch_set_size: 50}))
 print("Elapsed time: {:.6f}".format(time.time() - startTime))
--- trainingset.py
+++ trainingset.py
@@ -23,18 +23,18 @@ def labels():
     return result
 
 def next_batch(batchSize, repeat=False):
-    if batchSize > len(next_batch.labels): return None
+    if batchSize > num_set: batchSize = num_set
 
     batch_labels = np.array([]).reshape(-1, 64)
 
-    if next_batch.counter + batchSize <= len(next_batch.labels):        
+    if next_batch.counter + batchSize <= num_set:        
         batch_labels = next_batch.labels[next_batch.counter:next_batch.counter+batchSize]
-        next_batch.counter = (next_batch.counter + batchSize) % len(next_batch.labels)
+        next_batch.counter = (next_batch.counter + batchSize) % num_set
     else:
         batch_labels = np.append(batch_labels, next_batch.labels[next_batch.counter:], axis=0)
         if repeat == False: next_batch.counter = 0
         else:
-            next_batch.counter = (next_batch.counter + batchSize) % len(next_batch.labels)
+            next_batch.counter = (next_batch.counter + batchSize) % num_set
             batch_labels = np.append(batch_labels, next_batch.labels[0:next_batch.counter], axis=0)
 
     batch_charts = np.array([], dtype=int).reshape(-1, CHART_SIZE, 7) 
@@ -44,26 +44,64 @@ def next_batch(batchSize, repeat=False):
         endIndex = startIndex + CHART_SIZE
         batch_charts = np.append(batch_charts, [np.load(CHART_PATH + label[0])[startIndex:endIndex,:]], axis=0)
     
-    return batch_charts.reshape(-1, CHART_SIZE, 7, 1), batch_labels[:,3:].astype(int)
+    return batch_charts[:,:,2:].reshape(-1, CHART_SIZE, 1, 5), batch_labels[:,3:].astype(int)
 
+def next_test_batch(batchSize, repeat=False):
+    if batchSize > num_test_set: batchSize = num_test_set
+
+    batch_labels = np.array([]).reshape(-1, 64)
+
+    if next_test_batch.counter + batchSize <= num_test_set:
+        batch_labels = next_test_batch.labels[next_test_batch.counter:next_test_batch.counter+batchSize]
+        next_test_batch.counter = (next_test_batch.counter + batchSize) % num_test_set
+    else:
+        batch_labels = np.append(batch_labels, next_test_batch.labels[next_test_batch.counter:], axis=0)
+        if repeat == False: next_test_batch.counter = 0
+        else:
+            next_test_batch.counter = (next_test_batch.counter + batchSize) % num_test_set
+            batch_labels = np.append(batch_labels, next_test_batch.labels[0:next_test_batch.counter], axis=0)
+
+    batch_charts = np.array([], dtype=int).reshape(-1, CHART_SIZE, 7) 
+
+    for label in batch_labels[:,0:2]:
+        startIndex = int(label[1]) * 361
+        endIndex = startIndex + CHART_SIZE
+        batch_charts = np.append(batch_charts, [np.load(CHART_PATH + label[0])[startIndex:endIndex,:]], axis=0)
+    
+    return batch_charts[:,:,2:].reshape(-1, CHART_SIZE, 1, 5), batch_labels[:,3:].astype(int)
 
 
 def shuffle_batch_set():
     np.random.shuffle(next_batch.labels)
 
+
 next_batch.counter = 0
-next_batch.labels = labels()
-num_set = len(next_batch.labels)
+next_test_batch.counter = 0
+
+all_labels = labels()
+num_set = int(len(all_labels) * 0.8)
+num_test_set = len(all_labels) - num_set
+
+next_batch.labels = all_labels[:num_set]
+next_test_batch.labels = all_labels[num_set:]
+
+num_set = 200
 
 #np.set_printoptions(threshold=np.nan, linewidth=200)
 if __name__ == "__main__":
     startTime = time.time()
 
-    print("training set size: {:d}".format(len(next_batch.labels)))
+    print("training set size: {:d}".format(num_set))
+    print("test set size:     {:d}".format(num_test_set))
+
     shuffle_batch_set()
 
-    total_batch_set = int(math.ceil(len(next_batch.labels)/100.0))
+    total_batch_set = int(math.ceil(num_set/50.0))
     for i in range(total_batch_set):
-        print(next_batch(100))
+        print(next_batch(50))
+
+    total_test_batch_set = int(math.ceil(num_test_set/50.0))
+    for i in range(total_test_batch_set):
+        print(next_test_batch(50))
 
     print("Elapsed time: {:.6f}".format(time.time() - startTime))
